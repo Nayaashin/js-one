9a49a4b28c243b68d305a780298f02954d27186f
Nayaashin <aidana.ashirbekova@gmail.com>
2022-06-03 20:35:55 +0600

    first js

---
+++ script.js
@@ -0,0 +1,30 @@
+
+//1-задание 
+let e = 10; 
+let v = 3; 
+console.log(e % v); 
+ 
+//2-задание 
+let st = 2 ** 10; 
+console.log(st); 
+ 
+//3-задание 
+let c = 1; 
+let k = "2"; 
+let p = " белых медведей"; 
+console.log(c + k + p); 
+ 
+//4-задание 
+let a = 5; 
+let b = 10; 
+console.log(++a + --b); 
+ 
+//5-задание 
+const x = 15 * (4 + 25 - 55); 
+console.log(x); 
+ 
+//доп задание-1 
+let width = 10; 
+let heigth = 23; 
+let s = width * heigth; 
+console.log(s);
